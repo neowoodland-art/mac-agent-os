d65abbc5db9351eb82b4b45d91a074737e179a09
5kecheng <5kecheng@ghai.dev>
2026-05-31 16:52:08 +0800

    feat: gray out incompatible atomic ops based on last step's allows list

--- 05_tools/09_ave/scripts/audio_line/layer4_alignment/__init__.py
+++
@@ -1,8 +0,0 @@
-"""AudioLine 对齐层 — 歌词节拍对齐 / 分轨时间对齐"""
-from .lyric_aligner import LyricAligner, LyricTimeline
-from .stem_aligner import StemAligner, AlignResult
-
-__all__ = [
-    "LyricAligner", "LyricTimeline",
-    "StemAligner", "AlignResult",
-]
