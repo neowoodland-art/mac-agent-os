31a947a6d96b110621c03f38935eec7f04244356
5kecheng <5kecheng@ghai.dev>
2026-05-31 16:43:34 +0800

    fix: platform selector now actually filters atomic ops (douyin vs xiaohongshu)

--- 05_tools/07_matrix/scripts/matrix_modules/comment/xhs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-小红书评论语料模块
-"""
-from .corpus import get_comment, get_comments, random_comment, short_comment, long_comment
-
-__all__ = ["get_comment", "get_comments", "random_comment", "short_comment", "long_comment"]
